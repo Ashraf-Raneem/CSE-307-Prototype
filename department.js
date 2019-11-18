2fb572b1ca2f2a2e1d8e8d8c315216db017c3437
Ashraf-Raneem <48803665+Ashraf-Raneem@users.noreply.github.com>
2019-11-19 02:27:10 +0600

    after check

--- department.js
+++ department.js
@@ -1,6 +1,13 @@
 $(".myChart00").hide(); 
-
+$("#deptDropdown").hide(); 
+$("#schoolDropdown").click(function(){
+        $("#deptDropdown").show(); 
+})
 $(".loadBtn").click(function(){
         $(".myChart00").show(); 
         $(this).hide(); 
 })
+
+
+//do this 
+$("#elementId :selected").val();
