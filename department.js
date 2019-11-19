0965c8d24bb56f2054071fb5230011d1a89af8c1
Ashraf-Raneem <48803665+Ashraf-Raneem@users.noreply.github.com>
2019-11-20 01:54:42 +0600

    2nd last commit

--- department.js
+++ department.js
@@ -1,13 +1,20 @@
-$(".myChart00").hide(); 
-$("#deptDropdown").hide(); 
+$("#myChart00").hide(); 
+$("#myChart01").hide();
+$("#myChart02").hide();
+$("#myChart04").hide();
 $("#schoolDropdown").click(function(){
         $("#deptDropdown").show(); 
 })
 $(".loadBtn").click(function(){
-        $(".myChart00").show(); 
+        $("#myChart00").show(); 
+        $("#myChart01").show();
+        $("#myChart02").show();
         $(this).hide(); 
 })
 
-
+$(".comparisonLoad").click(function(){
+        $("#myChart04").show();
+        $(this).hide();
+})
 //do this 
 $("#elementId :selected").val();
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 $("#submitBtn").click(function(){
     window.open("dashboard.html"); 
 })
+$(".resetSubmit").click(function(){
+    window.alert("An e-mail has been sent to your mail")
+})
